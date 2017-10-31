dadb1f55c769f4cbd394e59255a9d9ee336dca9b
taekimm <taekimm@gmail.com>
2017-10-31 16:12:20 -0700

    test

--- src/app/yelp.service.ts
+++ src/app/yelp.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions } from '@angular/http';
-// import 'rxjs/Rx';
+import { Http } from '@angular/http';
+import 'rxjs';
 // import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/filter';
 
